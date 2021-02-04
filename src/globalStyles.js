74f868074dc9a785b55f0bd627f24a6d00303eb6
Sunil Bista <072bex444@ioe.edu.np>
2021-02-04 23:54:38 +0545

    added custom scrollbar

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -11,6 +11,27 @@ const GlobalStyles = createGlobalStyle`
     html, body{
         overflow-x: hidden;
     }
+
+    ::-webkit-scrollbar {
+        width: 10px;
+      }
+      
+      /* Track */
+      ::-webkit-scrollbar-track {
+        background: #f1f1f1; 
+        border-radius: 10px;
+      }
+       
+      /* Handle */
+      ::-webkit-scrollbar-thumb {
+        background:  #3d3d3d;
+        border-radius: 5px; 
+      }
+      
+      /* Handle on hover */
+      ::-webkit-scrollbar-thumb:hover {
+        background: #555; 
+      }
 `;
 
 export default GlobalStyles;
